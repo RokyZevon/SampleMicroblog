777bb869ef3bed5f8d8e59d4c2c3da0bd41a0b6a
RokyZevon <rk_zrj@hotmail.com>
2016-12-25 14:58:49 +0800

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,8 @@ class User < ApplicationRecord
                       uniqueness: { case_sensitive: false }
 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 },
+    allow_nil: true
 
   # 返回指定字符串的哈希摘要( digest )
 
